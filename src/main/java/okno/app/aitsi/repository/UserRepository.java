ff23b6a1c34c8ba0ef5a17d93737fa0486bd61eb
pawelmiszczyk <pawelmiszczyk1994@gmail.com>
2023-03-31 00:02:48 +0200

    Inicjalny commit.

---
+++ src/main/java/okno/app/aitsi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package okno.app.aitsi.repository;
+
+import okno.app.aitsi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
---
+++ src/test/java/okno/app/aitsi/AitsiApplicationTests.java
@@ -0,0 +1,13 @@
+package okno.app.aitsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitsiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
