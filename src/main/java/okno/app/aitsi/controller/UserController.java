2fc31f2d6cf8bd6133799d0ff5042e936a995591
pawelmiszczyk <pawelmiszczyk1994@gmail.com>
2023-03-31 00:45:41 +0200

    Udrożnienie komunikacji międzu gui i api.

--- src/main/java/okno/app/aitsi/controller/UserController.java
+++ src/main/java/okno/app/aitsi/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/")
 public class UserController {
